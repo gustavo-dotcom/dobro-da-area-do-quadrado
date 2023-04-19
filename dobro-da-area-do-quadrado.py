97d41a6b735d676e9a3711caba01bb8cba192967
gustavo906 <130378554+gustavo906@users.noreply.github.com>
2023-04-19 20:04:13 -0300

    Add files via upload

---
+++ dobro-da-area-do-quadrado.py
@@ -0,0 +1,5 @@
+lado = int(input("Informe o lado do quadrado em centímetros:\n"))
+area = lado**2
+dobroDaArea = area*2
+print("Área do quadrado em centímetros", area, "cm")
+print("O dobro da área do quadrado:", dobroDaArea, "cm")
